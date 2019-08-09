7175531a9ce72477a61f7178171ad3d283b15d79
unknown <eric.krief1@gmail.com>
2019-08-09 09:58:48 +0300

    First commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'stam'`, () => {
+  it(`should have as title 'Angular-Shop'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('stam');
+    expect(app.title).toEqual('Angular-Shop');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to stam!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Angular-Shop!');
   });
 });
